ccff1b021594976bc3776305d998cbead6a9730b
Klouz <yuliavydryk@gmail.com>
2025-09-11 02:21:13 +0300

    Add project files.

---
+++ NumCalc/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NumCalc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NumCalc/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NumCalc
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
