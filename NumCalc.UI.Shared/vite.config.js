cac661f87ef19c419c0549eeca8d6059b86b41c8
Klouz <yuliavydryk@gmail.com>
2026-01-19 00:06:38 +0200

    set up vite working

---
+++ NumCalc.UI.Shared/vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [
+        ViteImageOptimizer({
+            png: { quality: 80 },
+            jpeg: { quality: 75 },
+            webp: { lossless: true }
+        }),
+    ],
+    build: {
+        outDir: 'wwwroot/dist',
+        emptyOutDir: true,
+        sourcemap: true,
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'src/js/app.js'),
+                styles: path.resolve(__dirname, 'src/scss/main.scss')
+            },
+            output: {
+                entryFileNames: 'js/[name].js',
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+                        return 'css/[name][extname]';
+                    }
+                    return 'assets/[name][extname]';
+                }
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    }
+})
