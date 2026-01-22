87d2cbe12663fc3cd8430ea847b72edd79425b67
Klouz <yuliavydryk@gmail.com>
2026-01-23 00:06:43 +0200

    add new root finding page and use added models

---
+++ NumCalc.UI.Shared/Models/RootFindingModel.cs
@@ -0,0 +1,11 @@
+﻿using NumCalc.UI.Shared.Enums.Roots;
+
+namespace NumCalc.UI.Shared.Models;
+
+public class RootFindingModel
+{
+    public RootFindingMethod Method { get; set; }
+    public double StartPoint { get; set; }
+    public double EndPoint { get; set; }
+    public double Tolerance { get; set; } = 1e-4;
+}
---
+++ NumCalc.UI.Shared/Pages/RootFinding.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using NumCalc.UI.Shared.Models;
+using NumCalc.UI.Shared.Resources;
+
+namespace NumCalc.UI.Shared.Pages;
+
+public partial class RootFinding : ComponentBase
+{
+    [Inject] protected IStringLocalizer<Localization> Localizer { get; set; } = null!;
+    
+    private RootFindingModel Model = new();
+    private RootFindingComparisonModel ComparisonModel = new();
+}
