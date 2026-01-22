09473456c3a07f0ffc6f367a44ea98002bce17b6
Klouz <yuliavydryk@gmail.com>
2026-01-23 00:06:09 +0200

    add switch

---
+++ NumCalc.UI.Shared/Components/Switch.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NumCalc.UI.Shared.Components;
+
+public partial class Switch<TItem> : ComponentBase
+{
+    [Parameter] public IEnumerable<TItem> Data { get; set; } = [];
+    
+    [Parameter] public TItem? Value { get; set; }
+    [Parameter] public EventCallback<TItem?> ValueChanged { get; set; }
+    
+    [Parameter] public Func<TItem, string> TextField { get; set; } = x => x?.ToString() ?? string.Empty;
+
+    private bool IsSelected(TItem item)
+    {
+        return EqualityComparer<TItem>.Default.Equals(Value, item);
+    }
+
+    private async Task SelectItem(TItem item)
+    {
+        if (!IsSelected(item))
+        {
+            Value = item;
+            await ValueChanged.InvokeAsync(Value);
+        }
+    }
+}
