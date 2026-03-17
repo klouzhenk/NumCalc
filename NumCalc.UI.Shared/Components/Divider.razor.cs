d52e07a79171dc607df0438ceb45ab5183068a64
Klouz <yuliavydryk@gmail.com>
2026-03-18 00:38:16 +0200

    add and use devider component

---
+++ NumCalc.UI.Shared/Components/Divider.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NumCalc.UI.Shared.Components;
+
+public partial class Divider : ComponentBase
+{
+    [Parameter, EditorRequired] public string Text { get; set; } = null!;
+}
