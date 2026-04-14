8a10d2b53dc2c7a6e680d5f71ab059018e33b0c2
Klouz <yuliavydryk@gmail.com>
2026-04-14 17:05:18 +0300

    Add component to enter data necessary for the non-linear equation systems solving

---
+++ NumCalc.UI.Shared/Components/EquationSystems/EquationList.razor.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Components;
+using NumCalc.UI.Shared.Models.EquationSystems;
+
+namespace NumCalc.UI.Shared.Components.EquationSystems;
+
+public partial class EquationList : ComponentBase
+{
+    [Parameter] public int Size { get; set; }
+
+    private MathInput[] MathInputs { get; set; } = [];
+    private double[] InitialGuess { get; set; } = [];
+    private double Tolerance { get; set; } = 1e-6;
+    private int MaxIterations { get; set; } = 500;
+
+    protected override void OnParametersSet()
+    {
+        if (MathInputs.Length == Size) return;
+
+        MathInputs = new MathInput[Size];
+        InitialGuess = new double[Size];
+    }
+
+    public async Task<NonLinearSystemFormData> GetFormData()
+    {
+        var functions = new string[Size];
+        for (var i = 0; i < Size; i++)
+            functions[i] = await MathInputs[i].GetAsciiValue();
+
+        return new NonLinearSystemFormData
+        {
+            IterationFunctions = functions,
+            Variables = Enumerable.Range(1, Size).Select(i => $"x{i}").ToArray(),
+            InitialGuess = (double[])InitialGuess.Clone(),
+            Tolerance = Tolerance,
+            MaxIterations = MaxIterations
+        };
+    }
+}
