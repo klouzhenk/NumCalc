48f73075f818b2e37d53bb217c01ea827b16a230
Klouz <yuliavydryk@gmail.com>
2026-04-18 01:37:04 +0300

    add the topic info button in the header

--- NumCalc.UI.Shared/Components/Header.razor.cs
+++ NumCalc.UI.Shared/Components/Header.razor.cs
@@ -2,10 +2,28 @@
 
 namespace NumCalc.UI.Shared.Components;
 
-public partial class Header : ComponentBase
+public partial class Header : ComponentBase, IDisposable
 {
     [Inject] private NavigationManager NavigationManager { get; set; } = null!;
 
+    private string CurrentPath =>
+        new Uri(NavigationManager.Uri).AbsolutePath.TrimEnd('/');
+
+    protected override void OnInitialized()
+    {
+        NavigationManager.LocationChanged += OnLocationChanged;
+    }
+
+    private void OnLocationChanged(object? sender, Microsoft.AspNetCore.Components.Routing.LocationChangedEventArgs e)
+    {
+        StateHasChanged();
+    }
+
     private void OnHeaderLogoClick() =>
         NavigationManager.NavigateTo("/");
+
+    public void Dispose()
+    {
+        NavigationManager.LocationChanged -= OnLocationChanged;
+    }
 }
