ccc36ee0853a9cb30e42f0506f78c465450c3fb7
Klouz <yuliavydryk@gmail.com>
2026-04-18 01:17:37 +0300

    add header logo clickability

--- NumCalc.UI.Shared/Components/Header.razor.cs
+++ NumCalc.UI.Shared/Components/Header.razor.cs
@@ -4,4 +4,8 @@ namespace NumCalc.UI.Shared.Components;
 
 public partial class Header : ComponentBase
 {
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+
+    private void OnHeaderLogoClick() =>
+        NavigationManager.NavigateTo("/");
 }
