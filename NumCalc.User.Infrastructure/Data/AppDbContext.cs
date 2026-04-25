8d13804b2ef816d27216110195030833d00a63fd
Klouz <yuliavydryk@gmail.com>
2026-04-25 16:55:01 +0300

    add app dbcontext

---
+++ NumCalc.User.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NumCalc.User.Domain.Entities;
+
+namespace NumCalc.User.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+    public DbSet<CalculationHistory> CalculationHistoryRecords { get; set; }
+    public DbSet<SavedInput> SavedInputs { get; set; }
+    public DbSet<SavedPdfExport> SavedPdfExports { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
