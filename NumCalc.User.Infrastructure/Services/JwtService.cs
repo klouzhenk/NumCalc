11d31d27c9fdd9d996d3d87754b727d70eca5d30
Klouz <yuliavydryk@gmail.com>
2026-04-25 18:31:02 +0300

    implement jwt service

---
+++ NumCalc.User.Infrastructure/Services/JwtService.cs
@@ -0,0 +1,36 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using NumCalc.User.Application.Interfaces.Services;
+
+namespace NumCalc.User.Infrastructure.Services;
+
+public class JwtService(IConfiguration configuration) : IJwtService
+{
+    public string GenerateToken(Guid userId, string username)
+    {
+        var secret = configuration["JWT:Secret"]!;
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+            new Claim(ClaimTypes.Name, username)
+        };
+        
+        var expiryDays = int.Parse(configuration["JWT:ExpiryDays"]!);
+
+        var token = new JwtSecurityToken(
+            issuer: configuration["JWT:Issuer"],
+            audience: configuration["JWT:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddDays(expiryDays),
+            signingCredentials: credentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
