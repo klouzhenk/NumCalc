367a6c8a1331177a0df60a851e7b6d4b573a0984
Klouz <yuliavydryk@gmail.com>
2026-04-26 21:29:19 +0300

    add downloaded files overview page

---
+++ NumCalc.UI.Shared/Pages/SavedFiles.razor.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using NumCalc.UI.Shared.HttpServices.Interfaces;
+using NumCalc.UI.Shared.Models.User;
+using NumCalc.UI.Shared.Services.Interfaces;
+
+namespace NumCalc.UI.Shared.Pages;
+
+public partial class SavedFiles : BasePage<SavedFiles>
+{
+    [Inject] private ISavedFileApiService SavedFileApiService { get; set; } = null!;
+    [Inject] private IAuthStateService AuthStateService { get; set; } = null!;
+
+    private List<SavedFileMetadataDto>? Files { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (!AuthStateService.IsAuthenticated)
+        {
+            Navigation.NavigateTo("/login");
+            return;
+        }
+
+        await LoadAsync();
+    }
+
+    private async Task LoadAsync()
+    {
+        Files = await SafeExecuteAsync(() => SavedFileApiService.GetSavedFilesAsync());
+    }
+
+    private async Task DownloadAsync(Guid id, string fileName)
+    {
+        await SafeExecuteAsync(async () =>
+        {
+            var bytes = await SavedFileApiService.DownloadFileAsync(id);
+            if (bytes is null) return;
+            await JsRuntime.InvokeVoidAsync("NumCalc.downloadFile", fileName, Convert.ToBase64String(bytes));
+        });
+    }
+
+    private async Task DeleteAsync(Guid id)
+    {
+        await SafeExecuteAsync(async () =>
+        {
+            await SavedFileApiService.DeleteFileAsync(id);
+            await LoadAsync();
+        });
+    }
+}
