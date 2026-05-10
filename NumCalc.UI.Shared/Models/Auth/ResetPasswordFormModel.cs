073997a5196c0abc04c554021404a787545e78d9
Klouz <yuliavydryk@gmail.com>
2026-05-10 03:55:01 +0300

    added the page to reset password

---
+++ NumCalc.UI.Shared/Models/Auth/ResetPasswordFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NumCalc.UI.Shared.Models.Auth;
+
+public class ResetPasswordFormModel : IValidatableObject
+{
+    [Required(ErrorMessage = "PasswordRequired")]
+    public string NewPassword { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "ConfirmPasswordRequired")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (NewPassword != ConfirmPassword)
+            yield return new ValidationResult("PasswordsDoNotMatch", [nameof(ConfirmPassword)]);
+    }
+}
