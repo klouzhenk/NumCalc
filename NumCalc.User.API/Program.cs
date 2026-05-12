fd98aa95ecbbfd08541d359662858d1c03e16c90
klouz <101359531+klouzhenk@users.noreply.github.com>
2026-05-12 22:17:24 +0300

    docker deployment (#6)
    * setup the WebUI deployment via docker image
    
    * set up the calculation API deployment via Docker
    
    * set upt the user api deployment
    
    * add env-file example, add a docker compose file

--- NumCalc.User.API/Program.cs
+++ NumCalc.User.API/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 using NumCalc.User.API.Extensions;
 using NumCalc.User.API.Middlewares;
 using NumCalc.User.Infrastructure;
+using NumCalc.User.Infrastructure.Data;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,11 +25,23 @@ builder.Services.AddProblemDetails();
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseSerilogRequestLogging();
-app.UseSwagger();
-app.UseSwaggerUI();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseHttpsRedirection();
+}
+
 app.UseExceptionHandler(_ => { });
-app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
